8c84777216b933a0f41c396d97e25b9037b1e637
dingdongqin <dingdongqin@wedobest.com.cn>
2023-11-04 19:17:59 +0800

    Revert "first分支1"
    This reverts commit 89f5985f0ced6d7701ce95e58751ba7b343decbc.

--- GitDemo/assets/GitDemo/GitDemo.ts
+++ GitDemo/assets/GitDemo/GitDemo.ts
@@ -5,7 +5,6 @@ const { ccclass, property } = cc._decorator;
 export class GitDemo extends cc.Component {
     start() {
         console.log("gitdemo-master");
-        console.log("gitdemo-frist");
     }
 
     update(deltaTime: number) {}
