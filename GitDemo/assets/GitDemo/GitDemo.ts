9d33bd18e2cf052e68c54295bad8c80af8238867
lanxuaner <34474180+lanxuaner@users.noreply.github.com>
2023-11-06 22:09:10 +0800

    Update GitDemo.ts

--- GitDemo/assets/GitDemo/GitDemo.ts
+++ GitDemo/assets/GitDemo/GitDemo.ts
@@ -20,6 +20,7 @@ export class GitDemo extends cc.Component {
         console.log("gitdemo-main11");    
         console.log("gitdemo-test11");    
         console.log("gitdemo-main2");    
+        console.log("2023.11.6--main1");    
     }
 
     update(deltaTime: number) {}
